319a04a31b02b153d06bad922c72669a0575761e
康凯 <544529248@qq.com>
2018-06-19 22:20:43 +0800

    新建项目，配置桌面工作环境。

---
+++ SuperMap.ZS/SuperMap.ZS.Startup/DesktopPlugin.cs
@@ -0,0 +1,25 @@
+﻿using SuperMap.Desktop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SuperMap.ZS.Startup
+{
+    public class DesktopPlugin : Plugin
+    {
+        public DesktopPlugin(PluginInfo pluginInfo) : base(pluginInfo)
+        {
+        }
+
+        public override bool Initialize()
+        {
+            return base.Initialize();
+        }
+
+        public override bool ExitInstance()
+        {
+            return base.ExitInstance();
+        }
+    }
+}
