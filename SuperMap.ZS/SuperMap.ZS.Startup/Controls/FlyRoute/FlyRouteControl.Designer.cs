36033007620762a4265a0518e0359ee7888293c0
康凯 <544529248@qq.com>
2018-07-04 23:02:28 +0800

    1、飞行路线入库功能完成

--- SuperMap.ZS/SuperMap.ZS.Startup/Controls/FlyRoute/FlyRouteControl.Designer.cs
+++ SuperMap.ZS/SuperMap.ZS.Startup/Controls/FlyRoute/FlyRouteControl.Designer.cs
@@ -28,11 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.btnScreenTip = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.btnCommit = new System.Windows.Forms.Button();
             this.chkFlyRoute = new System.Windows.Forms.CheckedListBox();
+            this.btnSelectFiles = new System.Windows.Forms.Button();
+            this.contextMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.tsspPreview = new System.Windows.Forms.ToolStripMenuItem();
             this.groupBox1.SuspendLayout();
+            this.contextMenuStrip.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnScreenTip
@@ -50,11 +55,12 @@
             // 
             this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.btnSelectFiles);
             this.groupBox1.Controls.Add(this.btnCommit);
             this.groupBox1.Controls.Add(this.chkFlyRoute);
             this.groupBox1.Location = new System.Drawing.Point(12, 12);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(181, 337);
+            this.groupBox1.Size = new System.Drawing.Size(181, 402);
             this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "飞行路线";
@@ -62,12 +68,13 @@
             // btnCommit
             // 
             this.btnCommit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnCommit.Location = new System.Drawing.Point(100, 308);
+            this.btnCommit.Location = new System.Drawing.Point(100, 373);
             this.btnCommit.Name = "btnCommit";
             this.btnCommit.Size = new System.Drawing.Size(75, 23);
             this.btnCommit.TabIndex = 4;
             this.btnCommit.Text = "提  交";
             this.btnCommit.UseVisualStyleBackColor = true;
+            this.btnCommit.Click += new System.EventHandler(this.btnCommit_Click);
             // 
             // chkFlyRoute
             // 
@@ -75,6 +82,7 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.chkFlyRoute.CheckOnClick = true;
+            this.chkFlyRoute.ContextMenuStrip = this.contextMenuStrip;
             this.chkFlyRoute.FormattingEnabled = true;
             this.chkFlyRoute.Items.AddRange(new object[] {
             "1",
@@ -89,8 +97,33 @@
             "0"});
             this.chkFlyRoute.Location = new System.Drawing.Point(2, 19);
             this.chkFlyRoute.Name = "chkFlyRoute";
-            this.chkFlyRoute.Size = new System.Drawing.Size(175, 276);
+            this.chkFlyRoute.Size = new System.Drawing.Size(175, 340);
             this.chkFlyRoute.TabIndex = 3;
+            // 
+            // btnSelectFiles
+            // 
+            this.btnSelectFiles.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSelectFiles.Location = new System.Drawing.Point(19, 373);
+            this.btnSelectFiles.Name = "btnSelectFiles";
+            this.btnSelectFiles.Size = new System.Drawing.Size(75, 23);
+            this.btnSelectFiles.TabIndex = 5;
+            this.btnSelectFiles.Text = "导入文件";
+            this.btnSelectFiles.UseVisualStyleBackColor = true;
+            this.btnSelectFiles.Click += new System.EventHandler(this.btnSelectFiles_Click);
+            // 
+            // contextMenuStrip
+            // 
+            this.contextMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsspPreview});
+            this.contextMenuStrip.Name = "contextMenuStrip";
+            this.contextMenuStrip.Size = new System.Drawing.Size(101, 26);
+            // 
+            // tsspPreview
+            // 
+            this.tsspPreview.Name = "tsspPreview";
+            this.tsspPreview.Size = new System.Drawing.Size(180, 22);
+            this.tsspPreview.Text = "预览";
+            this.tsspPreview.Click += new System.EventHandler(this.tsspPreview_Click);
             // 
             // FlyRouteControl
             // 
@@ -103,6 +136,7 @@
             this.Load += new System.EventHandler(this.FlyRouteControl_Load);
             this.VisibleChanged += new System.EventHandler(this.FlyRouteControl_VisibleChanged);
             this.groupBox1.ResumeLayout(false);
+            this.contextMenuStrip.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -113,5 +147,8 @@
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Button btnCommit;
         private System.Windows.Forms.CheckedListBox chkFlyRoute;
+        private System.Windows.Forms.Button btnSelectFiles;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem tsspPreview;
     }
 }
--- SuperMap.ZS/SuperMap.ZS.Startup/Controls/FlyRoute/FlyRouteControl.cs
+++ SuperMap.ZS/SuperMap.ZS.Startup/Controls/FlyRoute/FlyRouteControl.cs
@@ -11,6 +11,8 @@ using System.Xml.Linq;
 using System.Diagnostics;
 using SuperMap.Desktop;
 using SuperMap.Realspace;
+using System.IO;
+using SuperMap.Data;
 
 namespace SuperMap.ZS.Startup
 {
@@ -49,6 +51,14 @@ namespace SuperMap.ZS.Startup
                 {
                     m_SceneControl = formScene.SceneControl;
                 }
+                if (lstData == null)
+                {
+                    lstData = new List<Label>();
+                }
+                foreach (Route route in m_SceneControl.Scene.FlyManager.Routes)
+                {
+                    lstData.Add(new Label { Text = route.Name, Tag = route });
+                }
                 LoadData();
             }
             catch (Exception ex)
@@ -61,18 +71,14 @@ namespace SuperMap.ZS.Startup
         {
             try
             {
-                if (lstData == null)
-                {
-                    lstData = new List<Label>();
-                }
-                foreach (Route route in m_SceneControl.Scene.FlyManager.Routes)
-                {
-                    lstData.Add(new Label { Text = route.Name, Tag = route });
-                }
+                chkFlyRoute.DataSource = null;
                 chkFlyRoute.DataSource = lstData;
                 chkFlyRoute.DisplayMember = "Text";
                 chkFlyRoute.ValueMember = "Tag";
-                chkFlyRoute.SetItemChecked(0, true);
+                if (chkFlyRoute.Items.Count > 0)
+                {
+                    chkFlyRoute.SetItemChecked(0, true);
+                }
             }
             catch (Exception ex)
             {
@@ -89,11 +95,103 @@ namespace SuperMap.ZS.Startup
                 {
                     lstData.Add(new Label { Text = route.Name, Tag = route });
                 }
-                chkFlyRoute.DataSource = null;
-                chkFlyRoute.DataSource = lstData;
-                chkFlyRoute.DisplayMember = "Text";
-                chkFlyRoute.ValueMember = "Tag";
-                chkFlyRoute.SetItemChecked(0, true);
+                LoadData();
+            }
+            catch (Exception ex)
+            {
+                Log.OutputBox(ex);
+            }
+        }
+
+        private void btnSelectFiles_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog open = new OpenFileDialog
+                {
+                    Filter = "飞行路线文件|*.fpf",
+                    InitialDirectory = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Desktop),
+                    Multiselect = true,
+                    Title = "选择飞行路线文件",
+                };
+                if (open.ShowDialog() == DialogResult.OK)
+                {
+                    foreach (string name in open.FileNames)
+                    {
+                        if (m_SceneControl.Scene.FlyManager.Routes.FromFile(name))
+                        {
+                            foreach (Route route in m_SceneControl.Scene.FlyManager.Routes)
+                            {
+                                lstData.Add(new Label { Text = route.Name, Tag = route });
+                            }
+                        }
+                    }
+                }
+                LoadData();
+            }
+            catch (Exception ex)
+            {
+                Log.OutputBox(ex);
+            }
+        }
+
+        private void btnCommit_Click(object sender, EventArgs e)
+        {
+            Recordset objRt = null;
+            try
+            {
+                if (chkFlyRoute.CheckedItems.Count == 0)
+                {
+                    m_Application.MessageBox.Show("请选择要提交的路线！");
+                    return;
+                }
+                objRt = (m_Application.Workspace.Datasources["CommonData"].Datasets["FlyRouteTable"] as DatasetVector).GetRecordset(false, CursorType.Dynamic);
+                Recordset.BatchEditor editor = objRt.Batch;
+                editor.MaxRecordCount = 10;
+                editor.Begin();
+                int index = 1;
+                foreach (object obj in chkFlyRoute.CheckedItems)
+                {
+                    Label label = obj as Label;
+                    Route route= label.Tag as Route;
+                    Dictionary<string, object> dic = new Dictionary<string, object>();
+                    dic.Add("RouteID", index++.ToString());
+                    dic.Add("RouteName", route.Name);
+                    dic.Add("Content", Encoding.Default.GetBytes(route.ToXML()));
+                    objRt.AddNew(null, dic);
+
+                }
+                editor.Update();
+                m_Application.MessageBox.Show("路线入库成功！");
+            }
+            catch (Exception ex)
+            {
+                Log.OutputBox(ex);
+                m_Application.MessageBox.Show("路线入库失败！");
+            }
+            finally
+            {
+                objRt.Close();
+                objRt.Dispose();
+            }
+        }
+
+        private void tsspPreview_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (chkFlyRoute.SelectedItems.Count == 0)
+                {
+                    m_Application.MessageBox.Show("请选择要预览的路线！");
+                    return;
+                }
+                Route route = (chkFlyRoute.SelectedItem as Label).Tag as Route;
+                Route existRoute = m_SceneControl.Scene.FlyManager.Routes[route.Name];
+                if (existRoute != null)
+                {
+                    m_SceneControl.Scene.FlyManager.Play();
+                    m_Application.MainForm.SelectedTabID = "SceneRouteManager";
+                }
             }
             catch (Exception ex)
             {
