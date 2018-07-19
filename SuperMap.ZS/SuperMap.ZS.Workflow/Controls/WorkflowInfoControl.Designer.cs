ca6ee7a0d65dee634670b81cdc7620b58a640ccf
康凯 <544529248@qq.com>
2018-07-19 22:34:25 +0800

    1、新增：工艺流程信息编辑

--- SuperMap.ZS/SuperMap.ZS.Workflow/Controls/WorkflowInfoControl.Designer.cs
+++ SuperMap.ZS/SuperMap.ZS.Workflow/Controls/WorkflowInfoControl.Designer.cs
@@ -41,12 +41,20 @@
             this.btn_Draw = new System.Windows.Forms.Button();
             this.label5 = new System.Windows.Forms.Label();
             this.btn_SelectColor = new System.Windows.Forms.Button();
+            this.lst_Items = new System.Windows.Forms.ListBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnSetCamera = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.cmb_Workflow = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(17, 17);
+            this.label1.Location = new System.Drawing.Point(14, 28);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(65, 12);
             this.label1.TabIndex = 0;
@@ -56,15 +64,16 @@
             // 
             this.txt_Name.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.txt_Name.Location = new System.Drawing.Point(91, 14);
+            this.txt_Name.Location = new System.Drawing.Point(88, 25);
             this.txt_Name.Name = "txt_Name";
-            this.txt_Name.Size = new System.Drawing.Size(167, 21);
+            this.txt_Name.Size = new System.Drawing.Size(149, 21);
             this.txt_Name.TabIndex = 1;
+            this.txt_Name.Leave += new System.EventHandler(this.txt_Name_Leave);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(17, 47);
+            this.label2.Location = new System.Drawing.Point(14, 58);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(65, 12);
             this.label2.TabIndex = 2;
@@ -75,9 +84,9 @@
             this.rtb_Description.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.rtb_Description.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.rtb_Description.Location = new System.Drawing.Point(19, 68);
+            this.rtb_Description.Location = new System.Drawing.Point(16, 79);
             this.rtb_Description.Name = "rtb_Description";
-            this.rtb_Description.Size = new System.Drawing.Size(239, 210);
+            this.rtb_Description.Size = new System.Drawing.Size(221, 130);
             this.rtb_Description.TabIndex = 3;
             this.rtb_Description.Text = "（描述信息在200字以内。）";
             this.rtb_Description.TextChanged += new System.EventHandler(this.rtb_Description_TextChanged);
@@ -87,7 +96,7 @@
             // lbl_Count
             // 
             this.lbl_Count.AutoSize = true;
-            this.lbl_Count.Location = new System.Drawing.Point(17, 281);
+            this.lbl_Count.Location = new System.Drawing.Point(14, 212);
             this.lbl_Count.Name = "lbl_Count";
             this.lbl_Count.Size = new System.Drawing.Size(83, 12);
             this.lbl_Count.TabIndex = 0;
@@ -96,7 +105,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(17, 320);
+            this.label3.Location = new System.Drawing.Point(14, 237);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(65, 12);
             this.label3.TabIndex = 2;
@@ -106,9 +115,9 @@
             // 
             this.txt_PlaySpeed.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.txt_PlaySpeed.Location = new System.Drawing.Point(91, 317);
+            this.txt_PlaySpeed.Location = new System.Drawing.Point(88, 234);
             this.txt_PlaySpeed.Name = "txt_PlaySpeed";
-            this.txt_PlaySpeed.Size = new System.Drawing.Size(126, 21);
+            this.txt_PlaySpeed.Size = new System.Drawing.Size(106, 21);
             this.txt_PlaySpeed.TabIndex = 1;
             this.txt_PlaySpeed.Text = "10";
             // 
@@ -116,7 +125,7 @@
             // 
             this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(223, 320);
+            this.label4.Location = new System.Drawing.Point(202, 237);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(35, 12);
             this.label4.TabIndex = 2;
@@ -125,7 +134,7 @@
             // btnScreenTip
             // 
             this.btnScreenTip.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnScreenTip.Location = new System.Drawing.Point(183, 622);
+            this.btnScreenTip.Location = new System.Drawing.Point(183, 648);
             this.btnScreenTip.Name = "btnScreenTip";
             this.btnScreenTip.Size = new System.Drawing.Size(75, 23);
             this.btnScreenTip.TabIndex = 4;
@@ -135,7 +144,7 @@
             // btn_Save
             // 
             this.btn_Save.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btn_Save.Location = new System.Drawing.Point(183, 411);
+            this.btn_Save.Location = new System.Drawing.Point(162, 300);
             this.btn_Save.Name = "btn_Save";
             this.btn_Save.Size = new System.Drawing.Size(75, 23);
             this.btn_Save.TabIndex = 4;
@@ -147,7 +156,7 @@
             // 
             this.btn_Draw.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.btn_Draw.Enabled = false;
-            this.btn_Draw.Location = new System.Drawing.Point(102, 411);
+            this.btn_Draw.Location = new System.Drawing.Point(81, 300);
             this.btn_Draw.Name = "btn_Draw";
             this.btn_Draw.Size = new System.Drawing.Size(75, 23);
             this.btn_Draw.TabIndex = 4;
@@ -158,7 +167,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(17, 360);
+            this.label5.Location = new System.Drawing.Point(14, 271);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(65, 12);
             this.label5.TabIndex = 2;
@@ -168,34 +177,102 @@
             // 
             this.btn_SelectColor.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.btn_SelectColor.Location = new System.Drawing.Point(91, 350);
+            this.btn_SelectColor.Location = new System.Drawing.Point(88, 261);
             this.btn_SelectColor.Name = "btn_SelectColor";
-            this.btn_SelectColor.Size = new System.Drawing.Size(126, 33);
+            this.btn_SelectColor.Size = new System.Drawing.Size(106, 33);
             this.btn_SelectColor.TabIndex = 5;
             this.btn_SelectColor.UseVisualStyleBackColor = true;
             this.btn_SelectColor.Click += new System.EventHandler(this.btn_SelectColor_Click);
+            // 
+            // lst_Items
+            // 
+            this.lst_Items.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lst_Items.FormattingEnabled = true;
+            this.lst_Items.ItemHeight = 12;
+            this.lst_Items.Location = new System.Drawing.Point(3, 17);
+            this.lst_Items.Name = "lst_Items";
+            this.lst_Items.Size = new System.Drawing.Size(237, 196);
+            this.lst_Items.TabIndex = 6;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.btnSetCamera);
+            this.groupBox1.Controls.Add(this.lst_Items);
+            this.groupBox1.Location = new System.Drawing.Point(15, 388);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(243, 244);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "分段数据";
+            // 
+            // btnSetCamera
+            // 
+            this.btnSetCamera.Location = new System.Drawing.Point(158, 215);
+            this.btnSetCamera.Name = "btnSetCamera";
+            this.btnSetCamera.Size = new System.Drawing.Size(75, 23);
+            this.btnSetCamera.TabIndex = 7;
+            this.btnSetCamera.Text = "设置视角";
+            this.btnSetCamera.UseVisualStyleBackColor = true;
+            this.btnSetCamera.Click += new System.EventHandler(this.btnSetCamera_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.lbl_Count);
+            this.groupBox2.Controls.Add(this.btn_SelectColor);
+            this.groupBox2.Controls.Add(this.txt_Name);
+            this.groupBox2.Controls.Add(this.btn_Draw);
+            this.groupBox2.Controls.Add(this.txt_PlaySpeed);
+            this.groupBox2.Controls.Add(this.btn_Save);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.rtb_Description);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Location = new System.Drawing.Point(15, 53);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(243, 329);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "流程信息";
+            // 
+            // cmb_Workflow
+            // 
+            this.cmb_Workflow.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmb_Workflow.FormattingEnabled = true;
+            this.cmb_Workflow.Location = new System.Drawing.Point(103, 17);
+            this.cmb_Workflow.Name = "cmb_Workflow";
+            this.cmb_Workflow.Size = new System.Drawing.Size(145, 20);
+            this.cmb_Workflow.TabIndex = 9;
+            this.cmb_Workflow.SelectedIndexChanged += new System.EventHandler(this.cmb_Workflow_SelectedIndexChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(29, 20);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(65, 12);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "现有流程：";
             // 
             // WorkflowInfoControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.btn_SelectColor);
-            this.Controls.Add(this.btn_Draw);
-            this.Controls.Add(this.btn_Save);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.cmb_Workflow);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.btnScreenTip);
-            this.Controls.Add(this.rtb_Description);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.txt_PlaySpeed);
-            this.Controls.Add(this.txt_Name);
-            this.Controls.Add(this.lbl_Count);
-            this.Controls.Add(this.label1);
             this.Name = "WorkflowInfoControl";
-            this.Size = new System.Drawing.Size(273, 660);
+            this.Size = new System.Drawing.Size(273, 686);
             this.Load += new System.EventHandler(this.WorkflowInfoControl_Load);
             this.VisibleChanged += new System.EventHandler(this.WorkflowInfoControl_VisibleChanged);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -216,5 +293,11 @@
         private System.Windows.Forms.Button btn_Draw;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Button btn_SelectColor;
+        private System.Windows.Forms.ListBox lst_Items;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnSetCamera;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ComboBox cmb_Workflow;
+        private System.Windows.Forms.Label label6;
     }
 }
--- SuperMap.ZS/SuperMap.ZS.Workflow/Controls/WorkflowInfoControl.cs
+++ SuperMap.ZS/SuperMap.ZS.Workflow/Controls/WorkflowInfoControl.cs
@@ -1,4 +1,6 @@
-﻿using SuperMap.Desktop;
+﻿using SuperMap.Data;
+using SuperMap.Desktop;
+using SuperMap.Realspace;
 using SuperMap.ZS.Common;
 using System;
 using System.Collections.Generic;
@@ -17,6 +19,9 @@ namespace SuperMap.ZS.Workflow
         private Desktop.Application m_Application;
         private DesktopSceneControl m_SceneControl;
         private string m_Description = "";
+
+        private List<Label> m_lstWorkflow;
+        private List<Label> m_lstItems;
 
         public WorkflowEditType WorkflowEditType { get; set; } = WorkflowEditType.New;
 
@@ -49,6 +54,44 @@ namespace SuperMap.ZS.Workflow
                 {
                     m_SceneControl = formScene.SceneControl;
                 }
+                m_lstWorkflow = new List<Label>();
+                m_lstItems = new List<Label>();
+                Recordset objRt = null;
+
+                try
+                {
+                    objRt = (m_Application.Workspace.Datasources["Resource"].Datasets["ArtCraftTable"] as DatasetVector).GetRecordset(false, CursorType.Static);
+                    objRt.MoveFirst();
+                    while (!objRt.IsEOF)
+                    {
+                        Label lbl = new Label
+                        {
+                            Text = Convert.ToString(objRt.GetFieldValue("CraftName")),
+                            Tag = objRt.GetFieldValue("CraftID")
+                        };
+                        m_lstWorkflow.Add(lbl);
+
+                        objRt.MoveNext();
+                    }
+                    cmb_Workflow.DataSource = m_lstWorkflow;
+                    cmb_Workflow.DisplayMember = "Text";
+                    if (cmb_Workflow.Items.Count > 0)
+                    {
+                        cmb_Workflow.SelectedIndex = 0;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Log.OutputBox(ex);
+                }
+                finally
+                {
+                    if (objRt != null)
+                    {
+                        objRt.Close();
+                        objRt.Dispose();
+                    }
+                }
             }
             catch (Exception ex)
             {
@@ -64,9 +107,11 @@ namespace SuperMap.ZS.Workflow
                 {
                     case WorkflowEditType.New:
                         btn_Draw.Enabled = false;
+                        cmb_Workflow.Enabled = false;
                         break;
                     case WorkflowEditType.Edit:
                         btn_Draw.Enabled = true;
+                        cmb_Workflow.Enabled = true;
                         break;
                 }
             }
@@ -143,7 +188,7 @@ namespace SuperMap.ZS.Workflow
         {
             try
             {
-
+                m_SceneControl.Scene.Layers[txt_Name.Text + "@SpaceData"].IsEditable = true;
             }
             catch (Exception ex)
             {
@@ -175,13 +220,185 @@ namespace SuperMap.ZS.Workflow
 
         private void btn_Save_Click(object sender, EventArgs e)
         {
+            Recordset objRt = null;
             try
             {
                 btn_Draw.Enabled = true;
+                objRt = (m_Application.Workspace.Datasources["Resource"].Datasets["ArtCraftTable"] as DatasetVector).GetRecordset(false, CursorType.Dynamic);
+                switch (WorkflowEditType)
+                {
+                    case WorkflowEditType.New:
+                        Dictionary<string, object> dic = new Dictionary<string, object>();
+                        dic.Add("CraftName", txt_Name.Text);
+                        dic.Add("Note", rtb_Description.Text.Equals(m_Tip) ? "" : rtb_Description.Text);
+                        dic.Add("CraftID", objRt.RecordCount + 1);
+                        dic.Add("PlaySpeed", txt_PlaySpeed.Text);
+                        dic.Add("Color", btn_SelectColor.BackColor.ToArgb().ToString());
+                        objRt.AddNew(null, dic);
+                        objRt.Update();
+                        DatasetVectorInfo info = new DatasetVectorInfo
+                        {
+                            Name = txt_Name.Text,
+                            Type = DatasetType.Line3D,
+                        };
+                        DatasetVector objDtV = m_Application.Workspace.Datasources["SpaceData"].Datasets.Create(info);
+                        m_SceneControl.Scene.Layers.Add(objDtV, new Layer3DSettingVector(), true);
+
+                        m_lstWorkflow.Add(new Label { Text = txt_Name.Text, Tag = objRt.RecordCount + 1 });
+                        cmb_Workflow.DataSource = null;
+                        cmb_Workflow.DataSource = m_lstWorkflow;
+                        cmb_Workflow.DisplayMember = "Text";
+                        if (cmb_Workflow.Items.Count > 0)
+                        {
+                            cmb_Workflow.SelectedIndex = cmb_Workflow.Items.Count - 1;
+                        }
+                        break;
+                    case WorkflowEditType.Edit:
+                        objRt.Edit();
+                        objRt.SetFieldValue("CraftName", txt_Name.Text);
+                        objRt.SetFieldValue("Note", rtb_Description.Text.Equals(m_Tip) ? "" : rtb_Description.Text);
+                        objRt.SetFieldValue("CraftID", objRt.RecordCount + 1);
+                        objRt.SetFieldValue("PlaySpeed", txt_PlaySpeed.Text);
+                        objRt.SetFieldValue("Color", btn_SelectColor.BackColor.ToArgb().ToString());
+                        objRt.Update();
+                        break;
+                }
             }
             catch (Exception ex)
             {
                 Log.OutputBox(ex);
+            }
+            finally
+            {
+                if (objRt != null)
+                {
+                    objRt.Close();
+                    objRt.Dispose();
+                }
+            }
+        }
+
+        private void cmb_Workflow_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Recordset objRt = null;
+            try
+            {
+                string id = (cmb_Workflow.SelectedItem as Label).Tag.ToString();
+                objRt = (m_Application.Workspace.Datasources["Resource"].Datasets["ArtCraftTable"] as DatasetVector).Query("CraftID='" + id + "'", CursorType.Static);
+                if (objRt.RecordCount > 0)
+                {
+                    txt_Name.Text = Convert.ToString(objRt.GetFieldValue("CraftName"));
+                    rtb_Description.Text = Convert.ToString(objRt.GetFieldValue("Note"));
+                    txt_PlaySpeed.Text = Convert.ToString(objRt.GetFieldValue("PlaySpeed"));
+                    string color = Convert.ToString(objRt.GetFieldValue("Color"));
+                    if (!string.IsNullOrEmpty(color))
+                    {
+                        btn_SelectColor.BackColor = Color.FromArgb(Convert.ToInt32(color.Split(',')[0]),
+                            Convert.ToInt32(color.Split(',')[1]), Convert.ToInt32(color.Split(',')[2]),
+                            Convert.ToInt32(color.Split(',')[3]));
+                    }
+
+                    objRt.Close();
+                    objRt.Dispose();
+
+                    objRt = (m_Application.Workspace.Datasources["Resource"].Datasets["ArtCraftTable"] as DatasetVector).GetRecordset(false, CursorType.Static);
+                    objRt.MoveFirst();
+                    while (!objRt.IsEOF)
+                    {
+                        Dictionary<string, object> dic = new Dictionary<string, object>
+                        {
+                            { "lon", objRt.GetFieldValue("Longitude") },
+                            { "lat", objRt.GetFieldValue("Latitude") },
+                            { "alt", objRt.GetFieldValue("Altitude") },
+                            { "tilt", objRt.GetFieldValue("Tilt") },
+                            { "heading", objRt.GetFieldValue("Heading") },
+                            { "geo", objRt.GetGeometry() }
+                        };
+                        bool flag = false;
+                        if (objRt.GetFieldValue("Longitude") == null)
+                        {
+                            flag = true;
+                        }
+                        Label lbl = new Label
+                        {
+                            Text = flag ? "第【" + objRt.GetID() + "】分段(无视角)" : "第【" + objRt.GetID() + "】分段",
+                            Tag = dic
+                        };
+                        m_lstItems.Add(lbl);
+
+                        objRt.MoveNext();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.OutputBox(ex);
+            }
+            finally
+            {
+                if (objRt != null)
+                {
+                    objRt.Close();
+                    objRt.Dispose();
+                }
+            }
+        }
+
+        private void txt_Name_Leave(object sender, EventArgs e)
+        {
+            try
+            {
+                if (m_Application.Workspace.Datasources["SpaceData"].Datasets.IsAvailableDatasetName(txt_Name.Text, DatasetType.Line3D))
+                {
+                    m_Application.MessageBox.Show("该流程已存在！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    btn_Save.Enabled = false;
+                }
+                else
+                {
+                    btn_Save.Enabled = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.OutputBox(ex);
+            }
+        }
+
+        private void btnSetCamera_Click(object sender, EventArgs e)
+        {
+            Recordset objRt = null;
+            try
+            {
+                if (lst_Items.SelectedItem == null)
+                {
+                    m_Application.MessageBox.Show("请选择分段数据！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    return;
+                }
+                objRt = (m_Application.Workspace.Datasources["SpaceData"].Datasets[txt_Name.Text] as DatasetVector).GetRecordset(false, CursorType.Dynamic);
+                objRt.Edit();
+                objRt.SetFieldValue("Longitude", m_SceneControl.Scene.Camera.Longitude);
+                objRt.SetFieldValue("Latitude", m_SceneControl.Scene.Camera.Latitude);
+                objRt.SetFieldValue("Altitude", m_SceneControl.Scene.Camera.Altitude);
+                objRt.SetFieldValue("Tilt", m_SceneControl.Scene.Camera.Tilt);
+                objRt.SetFieldValue("Heading", m_SceneControl.Scene.Camera.Heading);
+                objRt.Update();
+
+                if ((lst_Items.SelectedItem as Label).Text.Contains("("))
+                {
+                    (lst_Items.SelectedItem as Label).Text = (lst_Items.SelectedItem as Label).Text.Split('(')[0];
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.OutputBox(ex);
+            }
+            finally
+            {
+                if (objRt != null)
+                {
+                    objRt.Close();
+                    objRt.Dispose();
+                }
             }
         }
     }
